187e93adfcc05a4bd2a1824b1c76abdf61d634aa
Kiran Gurindagunta <kgurindagunta@salesforce.com>
2026-08-01 23:01:39 +0530

    Newchanges

--- WebToLeadProject/WebLead.js
+++ WebToLeadProject/WebLead.js
@@ -1,22 +1,35 @@
-function handleSubmit() {
-  let inputDate = document.querySelector(".inputDate");
-  let outputDate = document.querySelector(".outputDate");
+let checked = false
 
-  console.log(inputDate.value);
+function handleSubmit(event) {
+    if (checked) {
+        let inputDate = document.querySelector(".inputDate");
+        let outputDate = document.querySelector(".outputDate");
 
-  let formattedDate = new Date(inputDate.value).toLocaleDateString("en-IN");
-  outputDate.value = formattedDate;
+        console.log(inputDate.value);
+
+        let formattedDate = new Date(inputDate.value).toLocaleDateString("en-IN");
+        outputDate.value = formattedDate;
+    }
+    else{
+        alert('check the Recaptcha')
+        event.preventDefault();
+    }
+
 }
 
 function timestamp() {
-  var response = document.getElementById("g-recaptcha-response");
-  if (response == null || response.value.trim() == "") {
-    var elems = JSON.parse(
-      document.getElementsByName("captcha_settings")[0].value,
-    );
-    elems["ts"] = JSON.stringify(new Date().getTime());
-    document.getElementsByName("captcha_settings")[0].value =
-      JSON.stringify(elems);
-  }
+    var response = document.getElementById("g-recaptcha-response");
+    if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(
+            document.getElementsByName("captcha_settings")[0].value,
+        );
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value =
+            JSON.stringify(elems);
+    }
 }
 setInterval(timestamp, 500);
+
+function captchasuccess() {
+    checked = true
+}
