400dda9fe2bf812aa632a3993c207a7a29f57a75
Kiran Gurindagunta <kgurindagunta@salesforce.com>
2026-08-01 22:43:50 +0530

    Captcha Changes

--- WebToLeadProject/WebLead.js
+++ WebToLeadProject/WebLead.js
@@ -1,11 +1,22 @@
-function handleSubmit(){
+function handleSubmit() {
+  let inputDate = document.querySelector(".inputDate");
+  let outputDate = document.querySelector(".outputDate");
 
-    let inputDate = document.querySelector(".inputDate");
-    let outputDate = document.querySelector(".outputDate");
+  console.log(inputDate.value);
 
-    console.log(inputDate.value)
-
-    let formattedDate = new Date(inputDate.value).toLocaleDateString("en-IN")
-    outputDate.value = formattedDate;
-
+  let formattedDate = new Date(inputDate.value).toLocaleDateString("en-IN");
+  outputDate.value = formattedDate;
 }
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value,
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
